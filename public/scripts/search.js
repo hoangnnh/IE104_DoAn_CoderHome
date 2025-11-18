c25d3c3837cfeb71ab406ef881820165058b3f27
unknown <bmkhoi2005.work@gmail.com>
2025-11-19 00:19:39 +0700

    fix icon and responsive search

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,65 +1,68 @@
-
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get("q") || "";
 
 const scrollTopBtn = document.getElementById("scrollTopBtn");
-const tabs = document.querySelectorAll(".nav__items")
+const tabs = document.querySelectorAll(".nav__items");
 
 const userTab = document.getElementById("tab-users");
 const postTab = document.getElementById("tab-posts");
 const resultContainer = document.getElementById("main__content");
 
-
-
 async function fetchSearch(type, keyword) {
-    const res = await fetch(`/search?type=${type}&q=${encodeURIComponent(keyword)}`);
-    const data = await res.json();
-    return data;
+  const res = await fetch(
+    `/search?type=${type}&q=${encodeURIComponent(keyword)}`
+  );
+  const data = await res.json();
+  return data;
 }
 
 async function loadPosts() {
-    const posts = await fetchSearch("posts", query);
-    const container = document.querySelector(".main__content");
-    container.innerHTML = ``;
-    if(!posts.length) { 
-        container.innerHTML = `<p style="display: flex; justify-content: center; font-size: 2.5rem; font-weight: bold;">No post found!</p>`;
-        return;
-    }
-    container.innerHTML = posts
-        .map(
-            (post) =>
-                ` 
+  const posts = await fetchSearch("posts", query);
+  const container = document.querySelector(".main__content");
+  container.innerHTML = ``;
+  if (!posts.length) {
+    container.innerHTML = `<p style="display: flex; justify-content: center; font-size: 2.5rem; font-weight: bold;">No post found!</p>`;
+    return;
+  }
+  container.innerHTML = posts
+    .map(
+      (post) =>
+        ` 
         <hr class="divider">
         <article class="post__card">
         <div class="post__author">
-            <img src="${post.author.profilePicture
-                }" alt="author" class="post__author-img"/>
-                <a href="/profile/${post.author._id
-                }" class="post__author-name">${post.author.username || "Unknown"
-                }</a>
+            <img src="${
+              post.author.profilePicture
+            }" alt="author" class="post__author-img"/>
+                <a href="/profile/${
+                  post.author._id
+                }" class="post__author-name">${
+          post.author.username || "Unknown"
+        }</a>
         </div>
         <div class="post__left">
             <div class="post__left-text">
                 <div class="post__content">
-                    <a href="/post/${post._id}" class="post__content-title">${post.title
-                }</a>
+                    <a href="/post/${post._id}" class="post__content-title">${
+          post.title
+        }</a>
                     <p class="post__content-overview">${post.description}</p>
                 </div>
                 <div class="post__interact">
                     <div class="post__interact-meta">
                         <span class="created_date">${new Date(
-                    post.createdAt
-                ).toLocaleDateString("vi-VN", {
-                    day: "2-digit",
-                    month: "2-digit",
-                })}</span>
-                        <span><img src="/images/icons/heart-icon" class="react-icon-meta" style="display: inline;"/> 5.5K</span>
-                        <span><img src="/images/icons/comment-icon" class="react-icon-meta" style="display: inline;"/> 170</span>
+                          post.createdAt
+                        ).toLocaleDateString("vi-VN", {
+                          day: "2-digit",
+                          month: "2-digit",
+                        })}</span>
+                        <span><img src="/images/icons/heart-icon.svg" class="react-icon-meta" style="display: inline;"/> 5.5K</span>
+                        <span><img src="/images/icons/comment-icon.svg" class="react-icon-meta" style="display: inline;"/> 170</span>
                     </div>
                     <div class="post__interact-action">
-                        <button class="icon-btn"><img src="/images/minus-circle-outline.svg" class="react-icon"/></button>
-                        <button class="icon-btn"><img src="/images/bookmark-outline.svg" class="react-icon"/></button>
-                        <button class="icon-btn"><img src="/images/dots-horizontal.svg" class="react-icon"/></button>
+                        <button class="icon-btn"><img src="/images/icons/remove-icon.svg" class="react-icon"/></button>
+                        <button class="icon-btn"><img src="/images/icons/bookmark-icon.svg" class="react-icon"/></button>
+                        <button class="icon-btn"><img src="/images/icons/three-dots-icon.svg" class="react-icon"/></button>
                     </div>
                 </div>
             </div>
@@ -67,22 +70,22 @@ async function loadPosts() {
         </div>
         </article>
     `
-        )
-        .join("");
+    )
+    .join("");
 }
 async function loadUsers() {
-    const users = await fetchSearch("users", query);
-    const container = document.querySelector(".main__content");
-    container.innerHTML = ``;
-    
-    if(!users.length) { 
-        container.innerHTML = `<p style="display: flex; justify-content: center; font-size: 2.5rem; font-weight: bold;">No user found!</p>`;
-        return;
-    }
-    container.innerHTML = users.
-        map(
-            (user) =>
-                `
+  const users = await fetchSearch("users", query);
+  const container = document.querySelector(".main__content");
+  container.innerHTML = ``;
+
+  if (!users.length) {
+    container.innerHTML = `<p style="display: flex; justify-content: center; font-size: 2.5rem; font-weight: bold;">No user found!</p>`;
+    return;
+  }
+  container.innerHTML = users
+    .map(
+      (user) =>
+        `
             <div class="user__list-item">
                 <div class="user__info">
                     <img src="${user.profilePicture}" alt="Blogger's avatar" class="avatar">
@@ -97,44 +100,44 @@ async function loadUsers() {
             </div>
             <hr class="divider">
         `
-        ).join("");
+    )
+    .join("");
 }
 
 tabs.forEach((tab) => {
-    tab.addEventListener("click", async function (e) {
-        e.preventDefault();
+  tab.addEventListener("click", async function (e) {
+    e.preventDefault();
 
-        tabs.forEach((t) => t.classList.remove("active"));
-        this.classList.add("active");
+    tabs.forEach((t) => t.classList.remove("active"));
+    this.classList.add("active");
 
-        const type = this.dataset.tab;
+    const type = this.dataset.tab;
 
-        if (type === "post") {
-            loadPosts();
-            userTab.classList.remove("active");
-        } else if (type === "user") {
-            loadUsers();
-            userTab.classList.add("active");
-        }
-    });
+    if (type === "post") {
+      loadPosts();
+      userTab.classList.remove("active");
+    } else if (type === "user") {
+      loadUsers();
+      userTab.classList.add("active");
+    }
+  });
 });
-
 
 // Scroll top
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
+  if (window.scrollY > 200) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
 });
 
 scrollTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
 
 // Call Function
